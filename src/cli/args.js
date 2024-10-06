776744d169a2bc4787a4e7ff07a8bbe322e22c9c
Nika Antauri <antaurinika2002@gmail.com>
2024-10-06 15:35:38 +0400

    feat: add args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,16 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+
+  const propValuePairs = args.reduce((acc, arg, index) => {
+    if (arg.startsWith("--") && args[index + 1] !== undefined) {
+      const propName = arg.slice(2);
+      const value = args[index + 1];
+      acc.push(`${propName} is ${value}`);
+    }
+    return acc;
+  }, []);
+
+  console.log(propValuePairs.join(", "));
 };
 
 parseArgs();
