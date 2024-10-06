72e4f3c5e4f1b5da7b90199da08202c06155288b
Nika Antauri <antaurinika2002@gmail.com>
2024-10-06 15:35:50 +0400

    feat: add env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,9 @@
 const parseEnv = () => {
-    // Write your code here 
+  const prefix = "RSS_";
+  const envVars = Object.entries(process.env)
+    .filter(([varKey]) => varKey.startsWith(prefix))
+    .map(([envKey, envValue]) => `${envKey}=${envValue}`)
+    .join("; ");
+  console.log(envVars);
 };
-
 parseEnv();
