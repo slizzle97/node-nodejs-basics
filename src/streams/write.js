d204f8bf6a39f5a33476eda47c5f4612d0a50def
Nika Antauri <antaurinika2002@gmail.com>
2024-10-06 15:37:11 +0400

    feat: add write stream

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,15 @@
+import { createReadStream, createWriteStream, promises as fs } from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const source = path.join(__dirname, "files", "fileToWrite.txt");
+
+const writeStream = createWriteStream(source);
+
 const write = async () => {
-    // Write your code here 
+  process.stdin.pipe(writeStream);
 };
 
 await write();
