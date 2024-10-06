e20fad7b0712d5de4167c6d062347f045577544e
Nika Antauri <antaurinika2002@gmail.com>
2024-10-06 17:35:43 +0400

    feat: add child processes

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,13 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
+import { fork } from "child_process";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const script = path.join(__dirname, "files", "script.js");
+
+export const spawnChildProcess = async (args) => {
+  fork(script, args);
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(["arg1", "arg2", "arg3"]);
