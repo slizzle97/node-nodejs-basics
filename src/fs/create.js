f821669180d0ef961664cb7b1d7883791a846677
Nika Antauri <antaurinika2002@gmail.com>
2024-10-06 15:35:29 +0400

    feat: add create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,19 @@
+import { promises as fs } from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const dirPath = path.join(__dirname, "files");
+const filePath = path.join(dirPath, "fresh.txt");
+const text = "I am fresh and young";
+
 const create = async () => {
-    // Write your code here 
+  try {
+    await fs.writeFile(filePath, text, { flag: "wx" });
+  } catch {
+    throw new Error("FS operation failed");
+  }
 };
 
 await create();
