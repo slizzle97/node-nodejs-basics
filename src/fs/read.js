83193030f4bdda81c779acbd16753d00258f21d7
Nika Antauri <antaurinika2002@gmail.com>
2024-10-06 15:36:31 +0400

    feat: add read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,18 @@
+import { promises as fs } from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const source = path.join(__dirname, "files", "fileToRead.txt");
+
 const read = async () => {
-    // Write your code here 
+  try {
+    const data = await fs.readFile(source, { encoding: "UTF-8" });
+    console.log(data);
+  } catch {
+    throw new Error("FS operation failed");
+  }
 };
 
 await read();
