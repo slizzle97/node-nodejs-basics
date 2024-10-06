34cd0553deeb7304c10bcb5486ed2e2bbb1e5f38
Nika Antauri <antaurinika2002@gmail.com>
2024-10-06 15:37:20 +0400

    feat: add hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,20 @@
+import { promises as fs } from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+import { createHash } from "node:crypto";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const source = path.join(__dirname, "files", "fileToCalculateHashFor.txt");
+
 const calculateHash = async () => {
-    // Write your code here 
+  try {
+    const data = await fs.readFile(source);
+    const hash = createHash("sha256").update(data).digest("hex");
+    console.log(hash);
+  } catch {
+    throw new Error("FS operation failed");
+  }
 };
 
 await calculateHash();
