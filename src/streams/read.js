4799662e3fe541b9f723db6b8fb901c4a0ae8b65
Nika Antauri <antaurinika2002@gmail.com>
2024-10-06 15:37:00 +0400

    feat: add read stream

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,15 @@
+import { createReadStream, promises as fs } from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const source = path.join(__dirname, "files", "fileToRead.txt");
+
+const reader = createReadStream(source);
+
 const read = async () => {
-    // Write your code here 
+  reader.pipe(process.stdout);
 };
 
 await read();
