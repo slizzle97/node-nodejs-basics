70cbbc6b92a710165e8e72af3f7455fde40dc161
Nika Antauri <antaurinika2002@gmail.com>
2024-10-06 17:14:48 +0400

    feat: add worker

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,37 @@
+import { Worker } from "worker_threads";
+import os from "os";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const workerMethod = path.join(__dirname, "worker.js");
+
 const performCalculations = async () => {
-    // Write your code here
+  const cpuCount = os.cpus().length;
+  const results = [];
+
+  const createWorker = (number) => {
+    return new Promise((resolve) => {
+      const worker = new Worker(workerMethod, { workerData: number });
+      worker.on("message", (result) => {
+        resolve({ status: "resolved", data: result });
+      });
+
+      worker.on("error", () => {
+        resolve({ status: "error", data: null });
+      });
+    });
+  };
+  const promises = [];
+  for (let i = 0; i < cpuCount; i++) {
+    promises.push(createWorker(10 + i));
+  }
+
+  const workerResults = await Promise.all(promises);
+  workerResults.forEach((result) => results.push(result));
+
+  console.log(results);
 };
 
 await performCalculations();
